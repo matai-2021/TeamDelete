af95206ab2776bb7e070916aff8dca1095353f28
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2021-06-25 15:26:03 +1200

    style container

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 import { getStory } from '../apis/api'
 
-function App () {
+function App() {
   const [story, setStory] = useState('')
   const [input, setInput] = useState('I\' very happy today and')
 
-  function handleClick () {
+  function handleClick() {
     getStory(input)
       .then(text => {
         setStory(text)
@@ -16,21 +16,18 @@ function App () {
       })
   }
 
-  function handleChange (event) {
+  function handleChange(event) {
     setInput(event.target.value)
   }
 
   return (
     <>
       <div className='app'>
-        <p>The year is 3000. Alien, Exo has landed on a derelict planet Earth.
-          They explore the surroundings and come across the remains of a childrens book. The front cover has several authors
-          - Koko, Kris, Ahmed and Suji. Intrigued, they open the book and come across the first page of the book.
-        <quote>Once upon a time, there was a school of budding web developers who loved to play with Nintendo Switch, eat lunch and
-              go to Doolan Brothers. </quote>Exo was hooked on the first page but as they turned the page, they noticed the rest
-             of the pages were ripped out. Exo pulled out an app to finish the rest of the story...</p>
-        <p className='story'>{story}</p>
-        <textarea cols={50} rows={5} onChange={handleChange} value={input} />
+        <div className='container'>
+          <p className='story'>{story}</p>
+          <textarea cols={50} rows={5} onChange={handleChange} value={input} />
+
+        </div>
         <button onClick={handleClick}>Give me more!</button>
       </div>
     </>
